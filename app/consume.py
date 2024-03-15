af5f3e1dbb8d5934eed58137949f15e726394ffc
Joe Futrelle <jfutrelle@whoi.edu>
2024-03-15 10:50:36 -0400

    now authenticates to rabbitmq

--- app/consume.py
+++ app/consume.py
@@ -9,9 +9,11 @@ stdout_logger = Logger.stdout()
 
 logger = Logger.fanout([couch_logger, stdout_logger])
 
-amqp_host = os.environ.get('AMQP_HOST', 'localhost')
-amqp_exchange = os.environ.get('AMQP_EXCHANGE', 'provenance')
+amqp_host = os.environ.get('RABBITMQ_HOST', 'localhost')
+amqp_user = os.environ.get('RABBITMQ_USER', 'guest')
+amqp_password = os.environ.get('RABBITMQ_PASSWORD', 'guest')
+amqp_exchange = os.environ.get('RABBITMQ_EXCHANGE', 'provenance')
 
-subscriber = Subscriber(amqp_host, amqp_exchange, logger)
+subscriber = Subscriber(amqp_host, amqp_user, amqp_password, amqp_exchange, logger)
 
 subscriber.start()
